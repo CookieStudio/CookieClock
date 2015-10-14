c3698e5a89e25e24c60b5db6137ebac48796d9d3
Mariusz Saramak <mariusz.saramak@tomtom.com>
2015-10-14 16:00:52 +0200

    ORM lite save gamer result with unit test

--- app/src/androidTest/java/com/tomtom/cookieclock/ApplicationTest.java
+++ app/src/androidTest/java/com/tomtom/cookieclock/ApplicationTest.java
@@ -3,11 +3,57 @@ package com.tomtom.cookieclock;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+import com.tomtom.cookieclock.repository.GammerRepoHelper;
+import com.tomtom.cookieclock.repository.GammerResultDAO;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
+
+    public static final String GAMER1_NAME = "Mariusz";
+    public static final String GAMER1_SURNAME = "Saramak";
+    public static final String GAMMER_WITOLD_NAME = "Witold";
+
     public ApplicationTest() {
         super(Application.class);
+    }
+
+
+    public void testShouldSaveAndReadGammerResult(){
+        //GIVEN gammer repo helper with one result
+        GammerRepoHelper gammerRepoHelper = new GammerRepoHelper(getContext());
+
+        //WHEN save one result
+        gammerRepoHelper.saveGammerResult(GammerResultDAO.createGammerResult(GAMER1_NAME, GAMER1_SURNAME, 1000));
+        //THEN one result should be save
+        assert gammerRepoHelper.getResults().size() == 1;
+        assert gammerRepoHelper.getResults().get(0).getName().equals(GAMER1_NAME);
+        assert gammerRepoHelper.getResults().get(0).getSurname().equals(GAMER1_SURNAME);
+        assert gammerRepoHelper.getResults().get(0).getTimeinMs() == 1000l;
+    }
+
+    public void testShouldSaveFewResultAndDisplayThemInCorrectTimeOrder(){
+        //GIVEN gammer repo helper with few result
+        GammerRepoHelper gammerRepoHelper = new GammerRepoHelper(getContext());
+        //WHEN save one result
+        gammerRepoHelper.saveGammerResult(GammerResultDAO.createGammerResult(GAMER1_NAME, GAMER1_SURNAME, 1002));
+        gammerRepoHelper.saveGammerResult(GammerResultDAO.createGammerResult(GAMMER_WITOLD_NAME, "Olejniczak", 1001));
+        //THEN results should be get in correct order
+        assert gammerRepoHelper.getResults().size() == 2;
+        assert gammerRepoHelper.getResults().get(0).getName().equals(GAMMER_WITOLD_NAME);
+        assert gammerRepoHelper.getResults().get(1).getName().equals(GAMER1_NAME);
+
+    }
+
+    public void testShouldClearRepo(){
+        //GIVEN gammer repo helper with few result
+        GammerRepoHelper gammerRepoHelper = new GammerRepoHelper(getContext());
+        //WHEN save one result
+        gammerRepoHelper.saveGammerResult(GammerResultDAO.createGammerResult(GAMER1_NAME, GAMER1_SURNAME, 1002));
+        gammerRepoHelper.saveGammerResult(GammerResultDAO.createGammerResult(GAMMER_WITOLD_NAME, "Olejniczak", 1001));
+        //THEN clear should clear all table
+        gammerRepoHelper.clear();
+        assert gammerRepoHelper.getResults().size() == 0;
     }
 }
---
+++ app/src/main/java/com/tomtom/cookieclock/repository/GammerResultDAO.java
@@ -0,0 +1,76 @@
+package com.tomtom.cookieclock.repository;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+import java.util.UUID;
+
+/**
+ * Created by saramakm on 14-10-15.
+ */
+@DatabaseTable(tableName = "gammer")
+public class GammerResultDAO {
+
+
+    @DatabaseField(id = true)
+    private String uuid;
+
+
+    @DatabaseField
+    private String name;
+
+
+    @DatabaseField
+    private String surname;
+
+    @DatabaseField(columnName = "timeinms")
+    private long timeinms;
+
+    /**
+     * Dont use only for ORM
+     *
+     */
+    @Deprecated
+    public GammerResultDAO(){
+    }
+
+    public static GammerResultDAO createGammerResult(String name, String surname, long timeinms){
+        GammerResultDAO res =   new GammerResultDAO();
+        res.setUuid(UUID.randomUUID().toString());
+        res.name = name;
+        res.surname = surname;
+        res.timeinms = timeinms;
+        return res;
+    }
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public long getTimeinMs() {
+        return timeinms;
+    }
+
+    public void setTimeinMs(long timeinMs) {
+        this.timeinms = timeinMs;
+    }
+}
